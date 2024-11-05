8f239e23ea791505b8f3e77e4b3110d24ad06fec
Silla K <79395251+silla2807@users.noreply.github.com>
2024-11-05 14:36:06 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (49.50%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -3,7 +3,6 @@ class Solution {
         int k = 2;
 
         for (int i = 2; i < nums.length; i++) {
-            System.out.println(nums[i]+" "+nums[k - 2]);
             if (nums[i] != nums[k - 2]) {
                 nums[k] = nums[i];
                 k++;
